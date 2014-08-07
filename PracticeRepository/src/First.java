b1c0c1223d6ff68587114095b784cebdf0b1aa81
vanaja <vanaja.gan@gmail.com>
2014-08-07 19:11:08 -0400

    Comments added

---
+++ PracticeRepository/src/First.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	  
+		      String[] coins = { "A", "B", "C", "D", "E" };
+		      List l = new ArrayList();
+		      for (int i = 0; i < coins.length; i++)
+		         l.add(coins[i]);
+		      ListIterator liter = l.listIterator();
+		      System.out.println("Before reversal");
+		      while (liter.hasNext())
+		         System.out.println(liter.next());
+		      Collections.reverse(l);
+		      liter = l.listIterator();
+		      System.out.println("After reversal");
+		      while (liter.hasNext())
+		         System.out.println(liter.next());
+		   }
+		
+
+	}
+
+
